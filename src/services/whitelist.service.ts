56529144a6e8ee47fcd68e99774247366c4da02c
Greez <imgreez@Protonmail.com>
2025-02-25 13:01:05 +0330

    Update whitelist.service.ts

--- src/services/whitelist.service.ts
+++ src/services/whitelist.service.ts
@@ -2,12 +2,14 @@ import { Whitelist } from '../db/models/whitelist';
 
 // Example list of presale wallet addresses
 const presaleWallets = [
-    '0x48760E6DdA33fae87b17bf6a8351C495E6D0F436',
+    '0x48760e6dda33fae87b17bf6a8351c495e6d0f436',
     '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd',
-    '0x48760E6DdA33fae87b17bf6a8251C495E6D0F436',
-    '0x3F74F4cB76d5AD3BeC20D9e8e01bf997d68EdEb1',
-    '0x4458212a31e0577D0117795Bc2907F5d804EcF8e',
-    '0x4458212a31e0577d0117795bc2907f5d804ecf8e'
+    '0x48760e6dda33fae87b17bf6a8251c495e6d0f436',
+    '0x3f74f4cb76d5ad3bec20d9e8e01bf997d68edeb1',
+    '0x3f74f4cb76d5ad3bec20d9e8e01bf997d68edeb1',
+    '0x4458212a31e0577d0117795bc2907f5d804ecf8e',
+    '0x4458212a31e0577d0117795bc2907f5d804ecf8e',
+
     // Add more addresses as needed
 ];
 
