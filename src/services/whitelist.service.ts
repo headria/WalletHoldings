9ae2b98cd0acafcec32589cc093e11b59f046250
Greez <imgreez@Protonmail.com>
2025-02-25 12:25:43 +0330

    whitelist swagger info

--- src/services/whitelist.service.ts
+++ src/services/whitelist.service.ts
@@ -5,6 +5,7 @@ const presaleWallets = [
     '0x48760E6DdA33fae87b17bf6a8351C495E6D0F436',
     '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd',
     '0x48760E6DdA33fae87b17bf6a8251C495E6D0F436',
+    '0x3F74F4cB76d5AD3BeC20D9e8e01bf997d68EdEb1'
     // Add more addresses as needed
 ];
 
